c96e5dc791ffb92b82732f84e0a5628e1ec05fe2
Diego Lafuente <dlaf88@gmail.com>
2017-01-10 14:56:06 -0500

    User spec for creation and presence of last name and first name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+  	before do 
+  		@user = User.new(email: 'asdf@asdf.com',password: 'asdfasdf',password_confirmation: 'asdfasdf',first_name: 'Diego',last_name:'Lafuente')
+  	end 
+  	it 'can be created' do   		
+  		expect(@user).to be_valid
+  	end 
+  	it 'is not valid if first_name or last_name is nil' do 
+  		@user.first_name = nil
+  		@user.last_name = nil
+  		expect(@user).to_not be_valid
+  	end 
+  end 
 end
