f4bfb51a3d1e358bb37a8c247e9d3267c650a156
Diego Lafuente <dlaf88@gmail.com>
2017-01-10 15:42:32 -0500

    addedPost seeds file

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,6 +7,10 @@ describe 'Post' do
 			visit posts_path 
 			expect(page.status_code).to eq(200)
 		end 
+		it 'contains the word Post' do 
+			visit posts_path
+			expect(page).to have_content(/Posts/)
+		end 
 	end 
 
 end 
