1cbc5b47a3977f421b299a8ae6f5dad9dc19dfba
Diego Lafuente <dlaf88@gmail.com>
2017-01-11 11:05:37 -0500

    implemented creation process for posts and refactored

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,14 +3,30 @@ require 'rails_helper'
 describe 'Post' do 
 
 	describe 'index' do
-		it 'can be successfully reached' do 
+		before do 
 			visit posts_path 
+		end 
+		it 'can be successfully reached' do			
 			expect(page.status_code).to eq(200)
 		end 
-		it 'contains the word Post' do 
-			visit posts_path
+		it 'contains the word Post' do 			
 			expect(page).to have_content(/Posts/)
 		end 
 	end 
+
+	describe 'creation' do 
+		before do
+			visit new_post_path 
+		end 
+		it 'can reach a new form' do 			
+			expect(page.status_code).to eq(200)
+		end 
+		it 'can be created through form' do			
+			fill_in 'post[date]',with: Date.today
+			fill_in 'post[rationale]',with: 'Some rationale.'
+			click_on "Save"
+			expect(page).to have_content(/Some rationale./)
+		end 
+	end
 
 end 
