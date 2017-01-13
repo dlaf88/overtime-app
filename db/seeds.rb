af4476b80e85a13a383a862fac2ad4e9a3ba0de2
Diego Lafuente <dlaf88@gmail.com>
2017-01-13 16:52:13 -0500

    added user to seeds.rb and added user lastname to table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+@user = User.create!(first_name: 'Charrito',last_name: 'Javier',email: 'asdfasdf@gmail.com',password: 'asdfasdf',password_confirmation: 'asdfasdf')
+
 100.times do |post|
-	Post.create!(date: Time.now, rationale: "This is Number #{post + 1}")
+	Post.create!(date: Time.now, rationale: "This is Number #{post + 1}",user_id: @user.id)
 end 
