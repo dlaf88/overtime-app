23a75154a9e827d325e9795476e33abc85fe6b3e
Diego Lafuente <dlaf88@gmail.com>
2017-01-13 14:37:32 -0500

    added js for navbar active

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,18 @@
+function active_nav(loc){
+ if(loc.pathname == '/'){
+ 	var a = document.getElementById("home-li")
+ 	a.className += "active";
+ }else if (loc.pathname == '/posts'){
+ 	var a = document.getElementById("posts-li")
+ 	a.className += "active";
+
+ }	
+}
+
+function doc(){
+	var a = document.getElementById("posts-li");
+	console.log(a)
+}
+
+
+active_nav(location);
